e301eab2eb139460424949df60a621186a4047a8
RyanZanone <ryanzanone@ucsb.edu>
2026-02-20 14:41:34 -0800

    Added summary table to streamlit dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import data_analysis
 
 st.set_page_config(page_title="Loblaw Bio Analytics", layout="wide")
 st.title("Loblaw Bio: Clinical Trial Dashboard")
 st.markdown("Analysis of immune cell populations and treatment responses")
 
+st.divider()
+
+st.header("Initial Analysis - Data Overview")
+st.markdown("Summary table of the relative frequency of each cell population")
+
+summary_df = data_analysis.summary_table()
+st.dataframe(summary_df, use_container_width=True)
